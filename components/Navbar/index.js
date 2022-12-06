ac2ddc740fad278990ecbd972faeb1c383335497
vnctkevin <vnctkevin@gmail.com>
2022-12-06 14:06:11 +0700

    Add login logout implementation

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,26 +1,91 @@
+import axios from "axios";
 import Link from "next/link";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
-const Navbar = () => (
-  <nav className="w-full flex justify-center shadow-lg">
-    <div className="w-full max-w-[1440px] flex justify-between items-center p-4">
-      <div>
-        <p className="font-bold"><Link href="/">BooKantin</Link></p>
+import { API_URL } from "../../constants/api";
+
+import { useAuthContext } from "../../contexts/AuthContext";
+
+const Navbar = () => {
+  const { user, authLoading } = useAuthContext();
+  const [logoutLoading, setLogoutLoading] = useState(false);
+
+  const handleLogout = () => {
+    setLogoutLoading(true);
+
+    const token = localStorage.getItem("token");
+
+    axios
+      .post(
+        `${API_URL}/api/logout`,
+        {},
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      )
+      .then(() => {
+        localStorage.removeItem("token");
+
+        toast.success("Logout successful", {
+          duration: 4000,
+          position: "top-center",
+        });
+
+        window.location.reload();
+      })
+      .catch((err) => {
+        toast.error("Logout failed. Please try again.", {
+          duration: 4000,
+          position: "top-center",
+        });
+      })
+      .finally(() => setLogoutLoading(false));
+  };
+
+  return (
+    <nav className="w-full flex justify-center shadow-lg">
+      <div className="w-full max-w-[1440px] flex justify-between items-center p-4">
+        <div>
+          <p className="font-bold">
+            <Link href="/">BooKantin</Link>
+          </p>
+        </div>
+        {authLoading ? (
+          <p>Loading...</p>
+        ) : !!Object.keys(user ?? {}).length ? (
+          <div className="flex gap-x-4 items-center">
+            <p>Hello, {user?.first_name}!</p>
+
+            <button
+              className={`bg-red-500 ${
+                !logoutLoading && "hover:bg-red-700"
+              } text-white font-semibold py-2 px-4 rounded`}
+              onClick={() => handleLogout()}
+              disabled={logoutLoading}
+            >
+              {logoutLoading ? "Loading...." : "Logout"}
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-x-4">
+            <Link href="/login">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Login
+              </button>
+            </Link>
+            <Link href="/register">
+              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                Register
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
-      <div className="flex gap-x-4">
-        {/* use Link and a to redirect to /login */}
-        <Link href="/login">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Login
-          </button>
-        </Link>
-        <Link href="/register">
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Register
-          </button>
-        </Link>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navbar;
--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,20 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 import Layout from "../components/Layout";
 import { API_URL } from "../constants/api";
 
+import { useAuthContext } from "../contexts/AuthContext";
+
 const Login = () => {
-  const [type, setType] = useState(null);
-  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const router = useRouter();
+
+  const { setAuthLoading } = useAuthContext();
 
   const {
     register,
@@ -18,52 +23,93 @@ const Login = () => {
   } = useForm();
 
   const onSubmit = (data) => {
-    data.type = type;
-
     setLoading(true);
 
-    // axios
-    //   .post(`${API_URL}/api/login`, data)
-    //   .then(() => {
-        
-    // TODO: save token to local storage
-    
-    //     router.push("/");
-    //   })
-    //   .catch((err) => console.error(err))
-    //   .finally(() => {
-    //     setLoading(false);
-    //   });
+    axios
+      .post(`${API_URL}/api/login`, data)
+      .then((res) => {
+        const data = res.data;
+        const token = data?.token;
+
+        if (!!token) {
+          localStorage.setItem("token", token);
+
+          toast.success("Login successful", {
+            duration: 4000,
+            position: "top-center",
+          });
+
+          setAuthLoading(true);
+
+          router.push("/");
+        } else {
+          toast.error("Login failed. Please try again.", {
+            duration: 4000,
+            position: "top-center",
+          });
+        }
+      })
+      .catch((err) => {
+        toast.error(String(err), {
+          duration: 4000,
+          position: "top-center",
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
-    
     <Layout>
       <div className="flex justify-center w-full">
-        {page === 1 && (
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center gap-y-4">
-            <p className="font-semibold text-2xl text-center">
-              Login to BooKantin
-            </p>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="username" className="mb-1">
-                Username <span className="text-red-600">*</span>
-              </label> 
-              <input id="username" type="text" placeholder="Username" {...register("username", { required: true })} />
-              {errors.username && <p className="text-red-500">Username is required</p>}
-            </div>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="password" className="mb-1">
-                Password <span className="text-red-600">*</span>
-              </label>
-              <input id="password" type="password" placeholder="Password" {...register("password", { required: true })} />
-              {errors.password && <p className="text-red-500">Password is required</p>}
-            </div>
-            <button className="bg-[#e8e8e8] hover:bg-[#e0e0e0] font-semibold text-black max-w-full w-[400px] h-[50px] rounded-md">
-              Login
-            </button>
-          </form>
-        )}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col items-center gap-y-4"
+        >
+          <p className="font-semibold text-2xl text-center">
+            Login to BooKantin
+          </p>
+          <div className="flex flex-col max-w-full w-[400px]">
+            <label for="username" className="mb-1">
+              Username <span className="text-red-600">*</span>
+            </label>
+            <input
+              id="username"
+              className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+              type="text"
+              placeholder="Username"
+              {...register("username", { required: true })}
+            />
+            {errors.username && (
+              <p className="text-red-500">Username is required</p>
+            )}
+          </div>
+          <div className="flex flex-col max-w-full w-[400px]">
+            <label for="password" className="mb-1">
+              Password <span className="text-red-600">*</span>
+            </label>
+            <input
+              id="password"
+              className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+              type="password"
+              placeholder="Password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="text-red-500">Password is required</p>
+            )}
+          </div>
+          <button
+            className={`bg-green-500 ${
+              !loading && "hover:bg-green-700"
+            } font-semibold text-white max-w-full w-[400px] h-[50px] rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed`}
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Login"}
+          </button>
+        </form>
       </div>
     </Layout>
   );
