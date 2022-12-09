2ec0757c550126f22dd3d2f9fce4aa8d8a803b63
vnctkevin <vnctkevin@gmail.com>
2022-12-09 14:35:12 +0700

    Finalize CRUD Menu functionality'

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -49,17 +49,21 @@ const Menu = () => {
       });
   };
 
-  const editMenu = () => {
+  const editMenu = (data) => {
     const id = window.location.pathname.split("/")[2];
     const token = localStorage.getItem("token");
     axios
-      .put(`${API_URL}/api/menu/${id}`, data, {
+      .put(`${API_URL}/api/menu/${id}/`, data, {
         headers: {
           Authorization: `Token ${token}`,
-          },
-        })
+        },
+      })
       .then((res) => {
         setData(res.data);
+        toast.success("Menu berhasil diubah", {
+          duration: 4000,
+          position: "top-center",
+        });
       })
       .catch((err) => {
         toast.error(String(err), {
@@ -80,6 +84,10 @@ const Menu = () => {
       })
       .then((res) => {
         setData(res.data);
+        toast.success("Menu berhasil dihapus", {
+          duration: 4000,
+          position: "top-center",
+        });
       })
       .catch((err) => {
         toast.error(String(err), {
@@ -98,113 +106,111 @@ const Menu = () => {
     setPage(2);
   };
 
-
   return (
     <Layout>
       {authLoading ? (
         <p>Loading...</p>
       ) : !!Object.keys(data).length ? (
-      <div className="flex justify-center w-full">
-        {page === 1 && (
-          <div className="flex flex-col items-center gap-y-4">
-            <h1 className="font-semibold text-4xl text-center">{data.name}</h1>
-            <p className="font-semibold text-1xl text-center">
-              {" "}
-              Harga: {data.price}
-            </p>
-            <p className="font-semibold text-1xl text-center">
-              {" "}
-              Tipe Menu: {data.type}
-            </p>
-            <p className="font-semibold text-2xl text-center">
-              Dijual Oleh: 
-              {" "}
-              {data.seller.user.first_name} {" "}
-              {data.seller.user.last_name} 
-            </p>
-            <button
-              className="bg-[#e8e8e8] hover:bg-[#e0e0e0] font-semibold text-black max-w-full w-[400px] h-[50px] rounded-md"
-              onClick={() => gantiPage()}
-            >
-              {" "}
-              Edit{" "}
-            </button>
-            <button
-              className="bg-[#FF0000] hover:bg-[#800000] font-semibold text-white max-w-full w-[400px] h-[50px] rounded-md"
-              onClick={() => deleteClick()}
-            >
-              {" "}
-              Delete{" "}
-            </button>
-          </div>
-        )}
-        {page === 2 && (
-          <form
-            onSubmit={handleSubmit(editMenu)}
-            className="flex flex-col items-center gap-y-4 w-full px-4"
-          >
-            <p className="font-semibold text-2xl text-center mb-4">
-              Edit {data.name}
-            </p>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="name" className="mb-1">
-                Nama Makanan <span className="text-red-600">*</span>
-              </label>
-              <input
-                id="name"
-                className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-                placeholder="Tuliskan Nama Makanan"
-                {...register("name", { required: true })}
-              />
-              {errors.first_name && (
-                <span className="font-semibold text-red-500 text-sm">
-                  This field is required
-                </span>
-              )}
+        <div className="flex justify-center w-full">
+          {page === 1 && (
+            <div className="flex flex-col items-center gap-y-4">
+              <h1 className="font-semibold text-4xl text-center">
+                {data.name}
+              </h1>
+              <p className="font-semibold text-1xl text-center">
+                {" "}
+                Harga: {data.price}
+              </p>
+              <p className="font-semibold text-1xl text-center">
+                {" "}
+                Tipe Menu: {data.type}
+              </p>
+              <p className="font-semibold text-2xl text-center">
+                Dijual Oleh: {data.seller.user.first_name}{" "}
+                {data.seller.user.last_name}
+              </p>
+              <button
+                className="bg-[#e8e8e8] hover:bg-[#e0e0e0] font-semibold text-black max-w-full w-[400px] h-[50px] rounded-md"
+                onClick={() => gantiPage()}
+              >
+                {" "}
+                Edit{" "}
+              </button>
+              <button
+                className="bg-[#FF0000] hover:bg-[#800000] font-semibold text-white max-w-full w-[400px] h-[50px] rounded-md"
+                onClick={() => deleteClick()}
+              >
+                {" "}
+                Delete{" "}
+              </button>
             </div>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="price" className="mb-1">
-                Harga Makanan <span className="text-red-600">*</span>
-              </label>
-              <input
-                id="price"
-                className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-                placeholder="Harga Makanan"
-                {...register("price", { required: true })}
-              />
-              {errors.last_name && (
-                <span className="font-semibold text-red-500 text-sm">
-                  This field is required
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="type" className="mb-1">
-                Tipe <span className="text-red-600">*</span>
-              </label>
-              <input
-                id="price"
-                className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-                placeholder="Isi dengan FOOD/DRINK"
-                {...register("type", { required: true })}
-              />
-              {errors.last_name && (
-                <span className="font-semibold text-red-500 text-sm">
-                  This field is required
-                </span>
-              )}
-            </div>
-            <button
-              className="bg-[#e8e8e8] hover:bg-[#e0e0e0] font-semibold text-black max-w-full w-[400px] h-[50px] rounded-md"
-              onClick={() => editMenu()}
+          )}
+          {page === 2 && (
+            <form
+              onSubmit={handleSubmit(editMenu)}
+              className="flex flex-col items-center gap-y-4 w-full px-4"
             >
-              {" "}
-              Edit{" "}
-              {console.log(data)}
-            </button>
-          </form>
-        )}
-      </div>
+              <p className="font-semibold text-2xl text-center mb-4">
+                Edit {data.name}
+              </p>
+              <div className="flex flex-col max-w-full w-[400px]">
+                <label for="name" className="mb-1">
+                  Nama Makanan <span className="text-red-600">*</span>
+                </label>
+                <input
+                  id="name"
+                  className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+                  placeholder="Tuliskan Nama Makanan"
+                  {...register("name", { required: true })}
+                />
+                {errors.first_name && (
+                  <span className="font-semibold text-red-500 text-sm">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col max-w-full w-[400px]">
+                <label for="price" className="mb-1">
+                  Harga Makanan <span className="text-red-600">*</span>
+                </label>
+                <input
+                  id="price"
+                  className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+                  placeholder="Harga Makanan"
+                  {...register("price", { required: true })}
+                />
+                {errors.last_name && (
+                  <span className="font-semibold text-red-500 text-sm">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col max-w-full w-[400px]">
+                <label for="type" className="mb-1">
+                  Tipe <span className="text-red-600">*</span>
+                </label>
+                <input
+                  id="type"
+                  className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+                  placeholder="Isi dengan FOOD/DRINK"
+                  {...register("type", { required: true })}
+                />
+                {errors.last_name && (
+                  <span className="font-semibold text-red-500 text-sm">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <button
+                className="bg-[#e8e8e8] hover:bg-[#e0e0e0] font-semibold text-black max-w-full w-[400px] h-[50px] rounded-md"
+                onClick={() => editMenu()}
+              >
+                {" "}
+                Edit {console.log(data)}
+              </button>
+            </form>
+          )}
+        </div>
       ) : (
         <p>Not Authorized</p>
       )}
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -20,9 +20,8 @@ const Menu = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors }, 
+    formState: { errors },
   } = useForm();
-
 
   useEffect(() => {
     getMenu();
@@ -53,7 +52,6 @@ const Menu = () => {
   };
 
   const addMenu = (data) => {
-
     setLoading(true);
 
     const token = localStorage.getItem("token");
@@ -61,10 +59,14 @@ const Menu = () => {
       .post(`${API_URL}/api/menu/`, data, {
         headers: {
           Authorization: `Token ${token}`,
-          },
-          })
+        },
+      })
       .then((res) => {
         const data = res.data;
+        toast.success("Berhasil menambahkan menu", {
+          duration: 4000,
+          position: "top-center",
+        });
       })
       .catch((err) => {
         toast.error(String(err), {
@@ -77,7 +79,6 @@ const Menu = () => {
       });
   };
 
-  
   return (
     <Layout>
       {authLoading ? (
@@ -89,89 +90,89 @@ const Menu = () => {
               <p className="font-semibold text-2xl text-center pb-5">
                 Melihat Menu Milik {user?.first_name} {user?.last_name}
               </p>
-            <div className="flex justify-center flex-wrap gap-6 px-4">
-              {data.map((data, index) => (
-                <FoodCard key={index} {...data} />
-              ))}
-            </div>
-            <button
+              <div className="flex justify-center flex-wrap gap-6 px-4">
+                {data.map((data, index) => (
+                  <FoodCard key={index} {...data} />
+                ))}
+              </div>
+              <button
                 className="bg-[#e8e8e8] hover:bg-[#e0e0e0] font-semibold text-black max-w-full w-[400px] h-[50px] rounded-md"
                 onClick={() => gantiPage(2)}
               >
                 {" "}
                 Buat Menu Baru{" "}
               </button>
-        </div>
-        )} {page === 2 && (
-          <form
-            onSubmit={handleSubmit(addMenu)}
-            className="flex flex-col items-center gap-y-4 w-full px-4"
-          >
-            <p className="font-semibold text-2xl text-center mb-4">
-              Tambahkan Makanan Baru
-            </p>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="name" className="mb-1">
-                Nama Makanan <span className="text-red-600">*</span>
-              </label>
-              <input
-                id="name"
-                className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-                placeholder="Tuliskan Nama Makanan"
-                {...register("name", { required: true })}
-              />
-              {errors.first_name && (
-                <span className="font-semibold text-red-500 text-sm">
-                  This field is required
-                </span>
-              )}
             </div>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="price" className="mb-1">
-                Harga Makanan <span className="text-red-600">*</span>
-              </label>
-              <input
-                id="price"
-                className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-                placeholder="Harga Makanan"
-                {...register("price", { required: true })}
-              />
-              {errors.last_name && (
-                <span className="font-semibold text-red-500 text-sm">
-                  This field is required
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col max-w-full w-[400px]">
-              <label for="type" className="mb-1">
-                Tipe <span className="text-red-600">*</span>
-              </label>
-              <input
-                id="price"
-                className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-                placeholder="Isi dengan FOOD/DRINK"
-                {...register("type", { required: true })}
-              />
-              {errors.last_name && (
-                <span className="font-semibold text-red-500 text-sm">
-                  This field is required
-                </span>
-              )}
-            </div>
-            <button
-               className={`bg-green-500 ${
-                !loading && "hover:bg-green-700"
-              } font-semibold text-white max-w-full w-[400px] h-[50px] rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed`}
-              type="submit"
-              disabled={loading}
+          )}{" "}
+          {page === 2 && (
+            <form
+              onSubmit={handleSubmit(addMenu)}
+              className="flex flex-col items-center gap-y-4 w-full px-4"
             >
-              {loading ? "Loading..." : "Add Menu"}
-              
-            </button>
-          </form>
-            )}
-            </div>
-            ) : (
+              <p className="font-semibold text-2xl text-center mb-4">
+                Tambahkan Makanan Baru
+              </p>
+              <div className="flex flex-col max-w-full w-[400px]">
+                <label for="name" className="mb-1">
+                  Nama Makanan <span className="text-red-600">*</span>
+                </label>
+                <input
+                  id="name"
+                  className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+                  placeholder="Tuliskan Nama Makanan"
+                  {...register("name", { required: true })}
+                />
+                {errors.first_name && (
+                  <span className="font-semibold text-red-500 text-sm">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col max-w-full w-[400px]">
+                <label for="price" className="mb-1">
+                  Harga Makanan <span className="text-red-600">*</span>
+                </label>
+                <input
+                  id="price"
+                  className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+                  placeholder="Harga Makanan"
+                  {...register("price", { required: true })}
+                />
+                {errors.last_name && (
+                  <span className="font-semibold text-red-500 text-sm">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col max-w-full w-[400px]">
+                <label for="type" className="mb-1">
+                  Tipe <span className="text-red-600">*</span>
+                </label>
+                <input
+                  id="price"
+                  className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
+                  placeholder="Isi dengan FOOD/DRINK"
+                  {...register("type", { required: true })}
+                />
+                {errors.last_name && (
+                  <span className="font-semibold text-red-500 text-sm">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <button
+                className={`bg-green-500 ${
+                  !loading && "hover:bg-green-700"
+                } font-semibold text-white max-w-full w-[400px] h-[50px] rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed`}
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? "Loading..." : "Add Menu"}
+              </button>
+            </form>
+          )}
+        </div>
+      ) : (
         <p className="font-semibold text-2xl text-center">Anda belum login</p>
       )}
     </Layout>
