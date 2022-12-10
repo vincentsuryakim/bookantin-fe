382e3617c4dbe2ab1c094e54a062bad43502ff91
Vincent Suryakim <vincentsuryakim@gmail.com>
2022-12-10 20:47:34 +0700

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,40 +2,46 @@ import FoodCard from "../components/Card/Food/FoodCard";
 import Layout from "../components/Layout";
 
 import { dummyData } from "../constants/foodDummyData";
-import {useEffect, useRef, useState} from "react";
-import {API_URL} from "../constants/api";
+import { useEffect, useRef, useState } from "react";
+import { API_URL } from "../constants/api";
 import MenuList from "../components/Card/Food/MenuList";
 import axios from "axios";
 
 const Home = () => {
-    const [menus, setMenus] = useState([])
+  const [menus, setMenus] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getMenus();
-    }, [])
+  useEffect(() => {
+    getMenus();
+  }, []);
 
-    const getMenus = async () => {
-        const token = localStorage.getItem("token");
+  const getMenus = async () => {
+    const token = localStorage.getItem("token");
 
-        axios
-            .get(`${API_URL}/api/menu`, {
-                headers: {
-                    Authorization: `Token ${token}`,
-                },
-            })
-            .then((res) => {
-                setMenus(res.data);
-            })
-            .catch((err) => console.error(err))
-    };
+    axios
+      .get(`${API_URL}/api/menu`)
+      .then((res) => {
+        setMenus(res.data);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  };
 
-  return (
-    <Layout>
-      <div className="flex justify-center flex-wrap gap-6 px-4">
-          <MenuList menus ={menus} />
-      </div>
-    </Layout>
-  );
+  if (loading) {
+    return (
+      <Layout>
+        <p>Loading...</p>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <div className="flex justify-center flex-wrap gap-6 px-4">
+          <MenuList menus={menus} />
+        </div>
+      </Layout>
+    );
+  }
 };
 
 export default Home;
