beb35c908cf4b09e2124f8774000b6d710fbfb16
Vincent Suryakim <vincentsuryakim@gmail.com>
2022-12-10 14:49:59 +0700

    refactor

--- components/Dashboard/SellerDashboard.js
+++ components/Dashboard/SellerDashboard.js
@@ -1,78 +1,21 @@
 import axios from "axios";
-import FoodCard from "../../components/Card/Food";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { API_URL } from "../../constants/api";
-import { useAuthContext } from "../../contexts/AuthContext";
-import { useForm } from "react-hook-form";
+
+import SellerDashboardAddMenu from "./SellerDashboardAddMenu";
+import SellerDashboardGetMenu from "./SellerDashboardGetMenu";
 
 const SellerDashboard = () => {
-  const { user, authLoading } = useAuthContext();
-  const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [history, setHistory] = useState(null);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
   useEffect(() => {
-    if (page === 3) {
+    if (page === 2) {
       getSellerHistory();
-    } else {
-      getMenu();
     }
   }, [page]);
-
-  const getMenu = () => {
-    const token = localStorage.getItem("token");
-
-    axios
-      .get(`${API_URL}/api/menu`, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      })
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((err) => {
-        toast.error(String(err), {
-          duration: 4000,
-          position: "top-center",
-        });
-      });
-  };
-
-  const addMenu = (data) => {
-    setLoading(true);
-    const token = localStorage.getItem("token");
-    axios
-      .post(`${API_URL}/api/menu/`, data, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      })
-      .then((res) => {
-        const data = res.data;
-        toast.success("Berhasil menambahkan menu", {
-          duration: 4000,
-          position: "top-center",
-        });
-      })
-      .catch((err) => {
-        toast.error(String(err), {
-          duration: 4000,
-          position: "top-center",
-        });
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
 
   const getSellerHistory = () => {
     setLoading(true);
@@ -163,6 +106,21 @@ const SellerDashboard = () => {
               page === 2
                 ? "bg-gray-300 text-gray-900 active"
                 : "bg-gray-100 text-gray-700 hover:text-gray-700 hover:bg-gray-200"
+            } focus:ring-4 focus:outline-none focus:ring-blue-300 cursor-pointer`}
+            onClick={() => {
+              setLoading(true);
+              setPage(2);
+            }}
+          >
+            Add Menu
+          </div>
+        </li>
+        <li className="w-full">
+          <div
+            className={`inline-block p-4 w-full ${
+              page === 3
+                ? "bg-gray-300 text-gray-900 active"
+                : "bg-gray-100 text-gray-700 hover:text-gray-700 hover:bg-gray-200"
             } rounded-r-lg focus:ring-4 focus:outline-none focus:ring-blue-300 cursor-pointer`}
             onClick={() => {
               setLoading(true);
@@ -174,91 +132,8 @@ const SellerDashboard = () => {
         </li>
       </ul>
 
-      {page === 1 && (
-        <div className="flex flex-col items-center gap-y-4">
-          <p className="font-semibold text-2xl text-center pb-5">
-            Melihat Menu Milik {user?.first_name} {user?.last_name}
-          </p>
-          <div className="flex justify-center flex-wrap gap-6 px-4">
-            {data.map((data, index) => (
-              <FoodCard key={index} {...data} />
-            ))}
-          </div>
-          <button
-            className="bg-[#e8e8e8] hover:bg-[#e0e0e0] font-semibold text-black max-w-full w-[400px] h-[50px] rounded-md"
-            onClick={() => setPage(2)}
-          >
-            {" "}
-            Buat Menu Baru{" "}
-          </button>
-        </div>
-      )}
-      {page === 2 && (
-        <form
-          onSubmit={handleSubmit(addMenu)}
-          className="flex flex-col items-center gap-y-4 w-full px-4"
-        >
-          <p className="font-semibold text-2xl text-center mb-4">
-            Tambahkan Makanan Baru
-          </p>
-          <div className="flex flex-col max-w-full w-[400px]">
-            <label for="name" className="mb-1">
-              Nama Makanan <span className="text-red-600">*</span>
-            </label>
-            <input
-              id="name"
-              className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-              placeholder="Tuliskan Nama Makanan"
-              {...register("name", { required: true })}
-            />
-            {errors.first_name && (
-              <span className="font-semibold text-red-500 text-sm">
-                This field is required
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col max-w-full w-[400px]">
-            <label for="price" className="mb-1">
-              Harga Makanan <span className="text-red-600">*</span>
-            </label>
-            <input
-              id="price"
-              className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-              placeholder="Harga Makanan"
-              {...register("price", { required: true })}
-            />
-            {errors.last_name && (
-              <span className="font-semibold text-red-500 text-sm">
-                This field is required
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col max-w-full w-[400px]">
-            <label for="type" className="mb-1">
-              Tipe <span className="text-red-600">*</span>
-            </label>
-            <input
-              id="price"
-              className="bg-[#efefef] hover:bg-[#eaeaea] font-semibold text-black w-full h-[50px] px-4 rounded-md"
-              placeholder="Isi dengan FOOD/DRINK"
-              {...register("type", { required: true })}
-            />
-            {errors.last_name && (
-              <span className="font-semibold text-red-500 text-sm">
-                This field is required
-              </span>
-            )}
-          </div>
-          <button
-            className={`bg-green-500 ${
-              !loading && "hover:bg-green-700"
-            } font-semibold text-white max-w-full w-[400px] h-[50px] rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed`}
-            type="submit"
-          >
-            {loading ? "Loading..." : "Add Menu"}
-          </button>
-        </form>
-      )}
+      {page === 1 && <SellerDashboardGetMenu />}
+      {page === 2 && <SellerDashboardAddMenu />}
       {page === 3 && (
         <>
           {loading && <p>Loading...</p>}
